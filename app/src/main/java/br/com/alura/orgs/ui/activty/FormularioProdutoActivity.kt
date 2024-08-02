0ca4934910e541af4012d39bb909459b05f40498
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-08-01 23:07:52 -0300

    commit 2

--- app/src/main/java/br/com/alura/orgs/ui/activty/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activty/FormularioProdutoActivity.kt
@@ -8,8 +8,66 @@ import br.com.alura.orgs.R
 import br.com.alura.orgs.dao.ProdutoDAO
 import br.com.alura.orgs.model.Produto
 import java.math.BigDecimal
+import br.com.alura.orgs.databinding.ActivityFormularioProdutoBinding
 
-class FormularioProdutoActivity : AppCompatActivity(R.layout.formulario_produto_activity) {
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+    private val binding by lazy {
+        ActivityFormularioProdutoBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_formulario_produto)
+        setContentView(binding.root)
+        configuraBotaoSalvar()
+    }
+
+    private fun configuraBotaoSalvar() {
+        val botaoSalvar = binding.activityFormularioProdutoBotaoSalvar
+        val dao = ProdutoDAO()
+        botaoSalvar.setOnClickListener {
+            val produtoNovo = criaProduto()
+//            Log.i("FormularioProduto", "onCreate: $produtoNovo")
+            dao.adiciona(produtoNovo)
+            finish()
+//            Log.i("FormularioProduto", "onCreate: ${dao.buscaTodos()}")
+        }
+    }
+
+    private fun criaProduto(): Produto {
+        val campoNome = binding.activityFormularioProdutoNome
+        val nome = campoNome.text.toString()
+        val campoDescricao = binding.activityFormularioProdutoDescricao
+        val descricao = campoDescricao.text.toString()
+        val campoValor = binding.activityFormularioProdutoValor
+        val valorEmTexto = campoValor.text.toString()
+        val valor = if (valorEmTexto.isBlank()) {
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(valorEmTexto)
+        }
+
+        return Produto(
+            nome = nome,
+            descricao = descricao,
+            valor = valor,
+        )
+    }
+}
+
+/*
+package br.com.alura.orgs.ui.activty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import br.com.alura.orgs.R
+import br.com.alura.orgs.dao.ProdutoDAO
+import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_formulario_produto)
@@ -23,6 +81,7 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.formulario_produto_
             val produtoNovo = criaProduto()
 //            Log.i("FormularioProduto", "onCreate: $produtoNovo")
             dao.adiciona(produtoNovo)
+            finish()
 //            Log.i("FormularioProduto", "onCreate: ${dao.buscaTodos()}")
         }
     }
@@ -47,3 +106,4 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.formulario_produto_
         )
     }
 }
+*/
--- app/src/main/java/br/com/alura/orgs/ui/activty/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activty/ListaProdutosActivity.kt
@@ -9,8 +9,65 @@ import br.com.alura.orgs.R
 import br.com.alura.orgs.dao.ProdutoDAO
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import br.com.alura.orgs.databinding.ActivityListaProdutosBinding
 
-class ListaProdutosActivity : AppCompatActivity(R.layout.lista_produtos_activity) {
+class ListaProdutosActivity : AppCompatActivity() {
+    private val dao = ProdutoDAO()
+    private val adapter = ListaProdutosAdapter(
+        context = this,
+        produtos = dao.buscaTodos()
+    )
+
+    private val binding by lazy {
+        ActivityListaProdutosBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        configuraReciclerView()
+        configuraFab()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.atualiza(dao.buscaTodos())
+    }
+
+    private fun configuraFab() {
+        val fab = binding.activityListaProdutosFab
+        fab.setOnClickListener {
+            vaiParaFormularioProduto()
+        }
+    }
+
+    private fun vaiParaFormularioProduto() {
+        // Navegação entre telas
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun configuraReciclerView() {
+        val recyclerView = binding.activityListaProdutosRecyclerView
+//        Log.i("MainActivity", "onCreate: ${dao.buscaTodos()}")
+        recyclerView.adapter = adapter
+    }
+}
+
+/*
+package br.com.alura.orgs.ui.activty
+
+import android.content.Intent
+import android.os.Bundle
+//import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.R
+import br.com.alura.orgs.dao.ProdutoDAO
+import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListaProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos) {
     private val dao = ProdutoDAO()
     private val adapter = ListaProdutosAdapter(
         context = this,
@@ -47,3 +104,4 @@ class ListaProdutosActivity : AppCompatActivity(R.layout.lista_produtos_activity
         recyclerView.adapter = adapter
     }
 }
+*/
